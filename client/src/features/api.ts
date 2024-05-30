9d62f2200fa71b8fb5801ccc63cd86f94b593ea7
tk1351 <takakikitsui0111@gmail.com>
2024-05-31 07:14:19 +0900

    chore: add env api url

--- client/src/features/api.ts
+++ client/src/features/api.ts
@@ -3,16 +3,16 @@ import type { Status } from "../types/status.ts";
 import type { Task } from "../types/task.ts";
 
 export const getTasks = async () => {
-	const data = await fetch("http://localhost:8080/task");
+	const data = await fetch(`${import.meta.env.VITE_API_URL}/task`);
 	return (await data.json()) as Task[];
 };
 
 export const getIcons = async () => {
-	const data = await fetch("http://localhost:8080/icon");
+	const data = await fetch(`${import.meta.env.VITE_API_URL}/icon`);
 	return (await data.json()) as Icon[];
 };
 
 export const getStatus = async () => {
-	const data = await fetch("http://localhost:8080/status");
+	const data = await fetch(`${import.meta.env.VITE_API_URL}/status`);
 	return (await data.json()) as Status[];
 };
